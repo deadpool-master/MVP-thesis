08a8c7673a5def00b2126a3027a400f6df43e43b
Seif-Miehiar <Saif.muhiar97@gmail.com>
2019-06-01 22:09:11 +0300

    updating server

--- mvp-starter/server.js
+++ mvp-starter/server.js
@@ -1,2 +1,33 @@
 var express = require('express');
 var bcrypt = require('bcrypt');
+
+const port = 1997
+
+app.use(function (req, res, next) {
+    res.send('Hello World')
+  });
+  app.use(function (err, req, res, next) {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+  });
+
+
+  app.use(express.static(path.join(__dirname, 'public')))
+
+
+
+  
+
+  app.get('/', function (req, res) {
+    res.send('Welcome')
+  });
+
+
+
+app.get('/', function (req, res) {
+    res.send('hello world')
+  });
+
+  app.listen(port, () => {
+      console.log('listening on port ' + port + 'Happy Hacking!')
+  });
